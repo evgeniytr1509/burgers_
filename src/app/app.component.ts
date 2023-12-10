3255aa2ab484d90b6c5dbac872b20abf5f273df6
evgeniytr1509 <evgeniytr1509@gmail.com>
2023-12-10 14:41:37 +0200

    we

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+
 })
 export class AppComponent {
-  title = 'burgers';
+
+  form = this.fb.group({
+    order: ["", Validators.required],
+    mane: ["", Validators.required],
+    phone: ["", Validators.required],
+  })
+
+  constructor(private fb: FormBuilder) {
+
+  }
+  scrollTo(target: HTMLElement){
+    target.scrollIntoView({behavior: "smooth"});
+  }
+  confirmOrder(){
+    if (this.form.valid) {
+      alert("Спасибо за заказ! Мы скоро свяжемся с Вами!");
+    }
+  }
 }
